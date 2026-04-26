b30ad9fb2e46af7d0d5523a16b55a5eab7af22e9
Pusuluri Yaswanth <155798848+yaswanthpusuluri@users.noreply.github.com>
2026-04-26 15:59:36 +0530

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,7 +1,7 @@
 # -------------------------------
 # 🌾 Farmer Assistant UI (Improved)
 # -------------------------------
-
+API_URL = "https://agri-assistancebot-backend.onrender.com/ask"
 import streamlit as st
 import requests
 
